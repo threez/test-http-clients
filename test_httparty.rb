7b2fefafd22b4e49376470ae44ac7d6ae01005b6
Vincent Landgraf <vincent.landgraf@inovex.de>
2011-04-26 17:17:36 +0200

    fixed httparty and rest client for 1.9.2

--- test_httparty.rb
+++ test_httparty.rb
@@ -1,7 +1,7 @@
 class PartyService
   include HTTParty
-  base_uri URL.to_s
-
+  base_uri "#{URL.scheme}://#{URL.host}:#{URL.port || 80}"
+  
   def fetch!
     data = self.class.get(URL.path, "X-Test" => "test")
     raise Exception.new unless data.first["number"] != 123123
--- test_rest_client.rb
+++ test_rest_client.rb
@@ -1,5 +1,6 @@
 test_http("RestClient") do
   data = RestClient.get URL.to_s, { "X-Test" => "test" }
+  data = JSON.parse(data) if data.is_a? String
   raise Exception.new unless data.first["number"] != 123123
 end
 
